1b07afe62844eadbdb0134cd6c5fe2a0462fadde
Quentin SCHIFFERLE <dev.trope@gmail.com>
2024-05-12 13:17:43 +0200

    fix: Article vide au chargement de la page

--- resources/js/Pages/PostView.vue
+++ resources/js/Pages/PostView.vue
@@ -7,7 +7,7 @@
             <h1 class="lg:text-5xl sm:text-lg font-black text-slate-800 lg:leading-[60px] sm:leading-normal mb-4">{{ post.title }}</h1>
             <div class="text-sm text-slate-700">{{ moment(String(post.created_at )).format('LL') }}</div>
             <div class="text-xs text-slate-500 mb-2" v-if="post.created_at != post.updated_at">Mis à jour le {{ moment(String(post.updated_at )).format('LL') }} à {{ moment(String(post.updated_at )).format('LT') }}</div>
-            <div class="text-indigo-700 font-semibold text-sm">{{ post.category.name }}</div>
+            <div class="text-indigo-700 font-semibold text-sm">{{ post.category }}</div>
             <img :src="'../storage/' + post.picture" class="w-full aspect-video object-cover mb-16 shadow-2xl rounded-sm mt-8">
             <div class="text-lg text-slate-800 leading-6 mt-8" v-html="post.content"></div>
         </div>
@@ -16,12 +16,12 @@
 
 <script setup>
     import { useRoute } from 'vue-router'
-    import { ref, watch, onBeforeMount } from 'vue'
+    import { ref, onBeforeMount } from 'vue'
     import moment from 'moment'
     import 'moment/dist/locale/fr'
 
     const route = useRoute()
-    const post = ref(null)
+    const post = ref({})
 
     onBeforeMount(() =>
     {
